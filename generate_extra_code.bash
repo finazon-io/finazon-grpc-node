4be388718d2c96c4c40c1e69b7b2a77973796f66
miv <mivxxx@gmail.com>
2024-02-28 10:43:45 +0100

    Fix version parsing

--- generate_extra_code.bash
+++ generate_extra_code.bash
@@ -4,10 +4,11 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ### Implementation ##################################################
 function main {
   local -r version=$(cat ${SCRIPT_DIR}/package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[ ",]//g')
-  local -r versionMajor=$(printf %.1s "$version") # 1
+  local -r versionMajor=$(echo ${version} | cut -d. -f1)
+  local -r versionMinor=$(echo ${version} | cut -d. -f2)
 
   echo "export const FINAZON_GRPC_HOST: string;" > ${SCRIPT_DIR}/dist/constants.d.ts
-  echo "exports.FINAZON_GRPC_HOST='grpc-v${versionMajor}-0.finazon.io:443';" > ${SCRIPT_DIR}/dist/constants.js
+  echo "exports.FINAZON_GRPC_HOST='grpc-v${versionMajor}-${versionMinor}.finazon.io:443';" > ${SCRIPT_DIR}/dist/constants.js
 
   # generate service wrappers
   docker compose down --rmi local
